ec43f5fa21db050783828d7bc3a2b8977da51e2f
Jeongyeon Kim <93962252+yeon32@users.noreply.github.com>
2022-04-14 20:21:14 +0900

    Create 짝수와홀수.swift

---
+++ 짝수와홀수.swift
@@ -0,0 +1,13 @@
+func solution(_ num:Int) -> String {
+  
+    var result : String
+
+    if num % 2 == 0 {
+        result = "Even"
+    }
+    else {
+        result = "Odd"
+    }
+
+    return result
+}
