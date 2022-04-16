3d6b8cf9dd33f24c99538dea5ae5cba81cd43181
Jeongyeon Kim <93962252+yeon32@users.noreply.github.com>
2022-04-17 00:29:56 +0900

    Update 정수 제곱근 판별.swift

--- Programmers/Lv1/정수 제곱근 판별.swift
+++ Programmers/Lv1/정수 제곱근 판별.swift
@@ -1,1 +1,15 @@
+import Foundation
+
+func solution(_ n:Int64) -> Int64 {
+    var result: Int64 = 0 // 결과값을 담을 변수
+    let number = sqrt(Double(n))
+    
+    if number == Double(Int64(number)) {
+        result = Int64((number + 1) * (number + 1))
+    } else {
+        result = -1
+    }
+  
+    return result
+}
 
