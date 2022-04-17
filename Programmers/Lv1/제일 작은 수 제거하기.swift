176ffe04fea686bce4dcc06339cd0cdc35214a74
Jeongyeon Kim <93962252+yeon32@users.noreply.github.com>
2022-04-17 12:29:30 +0900

    Update 제일 작은 수 제거하기.swift

--- Programmers/Lv1/제일 작은 수 제거하기.swift
+++ Programmers/Lv1/제일 작은 수 제거하기.swift
@@ -1,1 +1,12 @@
+func solution(_ arr:[Int]) -> [Int] {
+    
+    var result = arr      
+    
+    let minValue = arr.min()! 
 
+    let minIndex = arr.index(of: minValue)!  
+   
+    result.remove(at: minIndex)
+  
+    return result.isEmpty ? [-1]: result
+}
