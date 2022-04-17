dfea2351585de4836da19301702efb8d8dbb9901
Jeongyeon Kim <93962252+yeon32@users.noreply.github.com>
2022-04-17 12:31:54 +0900

    Create 콜라츠 추측.swift

---
+++ Programmers/Lv1/콜라츠 추측.swift
@@ -0,0 +1,16 @@
+func solution(_ num:Int) -> Int {
+  var number = num
+  var count = 0
+  
+  while number != 1 && count <= 500 {
+    if number % 2 == 0 {
+      number = number / 2
+    } else {
+      number = number * 3 + 1
+    }
+    
+    count += 1
+  }
+  
+  return number == 1 ? count : -1
+}
