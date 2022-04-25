a40906b1116b1b9bb6983561917c51f4b2881d4e
김정연 <32jyeon@gmail.com>
2022-04-25 22:51:41 +0900

    Create 9498번.swift

---
+++ BAEKJOON/단계별로 풀어보기/9498번.swift
@@ -0,0 +1,13 @@
+let a = Int(readLine()!)!
+
+if a >= 90 {
+    print("A")
+} else if a >= 80 {
+    print("B")
+} else if a >= 70 {
+    print("C")
+} else if a >= 60 {
+    print("D")
+} else {
+    print("F")
+}
