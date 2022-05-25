e92d35f99d314523aeff9cf993b4ff1c610abd54
김정연 <32jyeon@gmail.com>
2022-05-25 14:32:30 +0900

    Update and rename 101번 Symmetric Tree.swift to 101. Symmetric Tree.swift

---
+++ LeetCode/101. Symmetric Tree.swift
@@ -0,0 +1,15 @@
+class Solution {
+    
+func isSymmetricTree(_ p: TreeNode?, _ q: TreeNode?) -> Bool {
+    if let p = p, let q = q {
+        return p.val == q.val && isSymmetricTree(p.left, q.right) &&
+        isSymmetricTree(p.right, q.left)
+        } else {
+        return p == nil && q == nil
+        }
+    }   
+ 
+func isSymmetric(_ root: TreeNode?) -> Bool {
+    return isSymmetricTree(root?.left, root?.right)
+    }
+}
