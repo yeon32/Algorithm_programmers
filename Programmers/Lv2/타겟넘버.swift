62b3239c08776d3397260f826f9a74c400bcf768
김정연 <32jyeon@gmail.com>
2022-05-30 14:45:07 +0900

    Update 타겟넘버.swift

--- Programmers/Lv2/타겟넘버.swift
+++ Programmers/Lv2/타겟넘버.swift
@@ -1,1 +1,20 @@
 
+func solution(_ numbers:[Int], _ target:Int) -> Int {
+  var count = 0
+  
+  func DFS(index: Int, sum: Int) {
+    if index == (numbers.count - 1) && sum == target { 
+      count += 1 
+      return 
+    }
+
+    guard index + 1 < numbers.count else { return }
+    
+    DFS(index: index+1, sum: sum + numbers[index + 1]) 
+    DFS(index: index+1, sum: sum - numbers[index + 1]) 
+  }
+  
+  DFS(index: -1, sum: 0)
+  
+  return count
+}
